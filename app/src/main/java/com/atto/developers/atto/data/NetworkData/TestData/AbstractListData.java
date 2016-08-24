9efe5eac36b31e79274459441a236486413f9678
jeon <wjscksdnd11@naver.com>
2016-08-24 17:03:43 +0900

    Data 클래스 이미지 배열 변수 변경될 예정,
    제작자 포트폴리오 남음.

--- app/src/main/java/com/atto/developers/atto/data/NetworkData/TestData/AbstractListData.java
+++ app/src/main/java/com/atto/developers/atto/data/NetworkData/TestData/AbstractListData.java
@@ -5,11 +5,24 @@ import java.io.Serializable;
 /**
  * Created by Tacademy on 2016-08-24.
  */
-abstract class  AbstractListData implements Serializable {
+abstract class  AbstractListData<T> implements Serializable {
 
     private String message;
-    private Object data;
+    private T data;
 
+    public String getMessage() {
+        return message;
+    }
 
+    public void setMessage(String message) {
+        this.message = message;
+    }
 
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
 }
