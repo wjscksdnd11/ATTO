d05a9e1cb41a42880e0d1cd2070fe6cd1a775594
jeon <wjscksdnd11@naver.com>
2016-08-24 13:43:57 +0900

    Data 클래스 중복 코드  통합

---
+++ app/src/main/java/com/atto/developers/atto/data/NetworkData/datatest/Nego.java
@@ -0,0 +1,34 @@
+package com.atto.developers.atto.data.NetworkData.datatest;
+
+import com.atto.developers.atto.data.NetworkData.ListData.PagingData;
+
+public class Nego implements java.io.Serializable {
+    private static final long serialVersionUID = 8180847119468994447L;
+    private NegoData[] data;
+    private PagingData paging;
+    private String message;
+
+    public NegoData[] getData() {
+        return this.data;
+    }
+
+    public void setData(NegoData[] data) {
+        this.data = data;
+    }
+
+    public PagingData getPaging() {
+        return this.paging;
+    }
+
+    public void setPaging(PagingData paging) {
+        this.paging = paging;
+    }
+
+    public String getMessage() {
+        return this.message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
---
+++ app/src/main/java/com/atto/developers/atto/data/NetworkData/datatest/NegoDataMaker_info.java
@@ -0,0 +1,32 @@
+package com.atto.developers.atto.data.NetworkData.datatest;
+
+public class NegoDataMaker_info implements java.io.Serializable {
+    private static final long serialVersionUID = -2961397907002658023L;
+    private String maker_profile_img;
+    private String maker_name;
+    private String maker_score;
+
+    public String getMaker_profile_img() {
+        return this.maker_profile_img;
+    }
+
+    public void setMaker_profile_img(String maker_profile_img) {
+        this.maker_profile_img = maker_profile_img;
+    }
+
+    public String getMaker_name() {
+        return this.maker_name;
+    }
+
+    public void setMaker_name(String maker_name) {
+        this.maker_name = maker_name;
+    }
+
+    public String getMaker_score() {
+        return this.maker_score;
+    }
+
+    public void setMaker_score(String maker_score) {
+        this.maker_score = maker_score;
+    }
+}
