7c09dbf75950e598ba364e9fff676a33c8cb9bc5
jeon <wjscksdnd11@naver.com>
2016-08-24 14:00:48 +0900

    협상 데이터 추가
    - add  NegoData pakage

--- app/src/main/java/com/atto/developers/atto/data/NetworkData/NegoData/NegoDataMaker_info.java
+++ app/src/main/java/com/atto/developers/atto/data/NetworkData/NegoData/NegoDataMaker_info.java
@@ -1,4 +1,4 @@
-package com.atto.developers.atto.data.NetworkData.datatest;
+package com.atto.developers.atto.data.NetworkData.NegoData;
 
 public class NegoDataMaker_info implements java.io.Serializable {
     private static final long serialVersionUID = -2961397907002658023L;
