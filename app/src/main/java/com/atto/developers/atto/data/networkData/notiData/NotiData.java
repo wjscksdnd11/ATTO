fdeb30f0cb447f45c779501351d68207023df8a8
jeon <wjscksdnd11@naver.com>
2016-08-25 11:13:52 +0900

    pakage명 변경

---
+++ app/src/main/java/com/atto/developers/atto/data/networkData/notiData/NotiData.java
@@ -0,0 +1,32 @@
+package com.atto.developers.atto.data.networkData.notiData;
+
+public class NotiData implements java.io.Serializable {
+    private static final long serialVersionUID = 6722883179600308245L;
+    private String trade_id;
+    private String notice_contents;
+    private String notice_id;
+
+    public String getTrade_id() {
+        return this.trade_id;
+    }
+
+    public void setTrade_id(String trade_id) {
+        this.trade_id = trade_id;
+    }
+
+    public String getNotice_contents() {
+        return this.notice_contents;
+    }
+
+    public void setNotice_contents(String notice_contents) {
+        this.notice_contents = notice_contents;
+    }
+
+    public String getNotice_id() {
+        return this.notice_id;
+    }
+
+    public void setNotice_id(String notice_id) {
+        this.notice_id = notice_id;
+    }
+}
---
+++ app/src/main/java/com/atto/developers/atto/data/networkData/portfolioData/PortfolioListData.java
@@ -0,0 +1,34 @@
+package com.atto.developers.atto.data.networkData.portfolioData;
+
+import com.atto.developers.atto.data.networkData.listData.PagingData;
+
+public class PortfolioListData implements java.io.Serializable {
+    private static final long serialVersionUID = 3135415551461301466L;
+    private PortfolioData[] data;
+    private PagingData paging;
+    private String message;
+
+    public PortfolioData[] getData() {
+        return this.data;
+    }
+
+    public void setData(PortfolioData[] data) {
+        this.data = data;
+    }
+
+    public PagingData getPaging() {
+        return this.paging;
+    }
+
+    public void setPaging(PagingData paging) {
+        this.paging = paging;
+    }
+
+    public String getMessage() {
+        return this.message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
