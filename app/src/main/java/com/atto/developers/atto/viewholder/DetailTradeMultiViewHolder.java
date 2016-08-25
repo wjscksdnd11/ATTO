68c0259adf312ba9fdf237995b313affa8216bd5
Seok Min Choi <pcgo118@gmail.com>
2016-08-25 18:14:02 +0900

    DetailTradeActivity 액티비티만 일단 수정

---
+++ app/src/main/java/com/atto/developers/atto/viewholder/DetailTradeMultiViewHolder.java
@@ -0,0 +1,13 @@
+package com.atto.developers.atto.viewholder;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+/**
+ * Created by Tacademy on 2016-08-25.
+ */
+public class DetailTradeMultiViewHolder extends RecyclerView.ViewHolder {
+    public DetailTradeMultiViewHolder(View itemView) {
+        super(itemView);
+    }
+}
