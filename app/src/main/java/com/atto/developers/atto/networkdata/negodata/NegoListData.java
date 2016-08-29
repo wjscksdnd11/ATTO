2e121cc06f6cd58e69f66da6face3da47aeef2ef
jeon <wjscksdnd11@naver.com>
2016-08-29 10:46:25 +0900

    pakage error수정

--- app/src/main/java/com/atto/developers/atto/networkdata/negodata/NegoListData.java
+++ app/src/main/java/com/atto/developers/atto/networkdata/negodata/NegoListData.java
@@ -1,7 +1,6 @@
-package com.atto.developers.atto.data.networkdata.negodata;
+package com.atto.developers.atto.networkdata.negodata;
 
 import com.atto.developers.atto.networkdata.listdata.PagingData;
-import com.atto.developers.atto.networkdata.negodata.NegoData;
 
 public class NegoListData implements java.io.Serializable {
     private static final long serialVersionUID = 8180847119468994447L;
