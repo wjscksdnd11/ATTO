a38b71d25fdca8fa14aea4a5aea3d0eeb05715dd
Park Museong <goodness897@gmail.com>
2016-08-29 17:46:59 +0900

    ratingbar

--- app/src/main/java/com/atto/developers/atto/MyPageSetProfileActivity.java
+++ app/src/main/java/com/atto/developers/atto/MyPageSetProfileActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
@@ -30,8 +31,10 @@ public class MyPageSetProfileActivity extends AppCompatActivity {
         TextView textView = (TextView) toolbar.findViewById(R.id.text_title);
         textView.setText(R.string.activity_my_page_set_profile);
 
+        // 주소 텍스트 뷰
         addressView = (TextView)findViewById(R.id.text_profile_set_address);
 
+        // 주소 호출 버튼
         ImageButton imgBtn = (ImageButton) findViewById(R.id.btn_img_address);
         imgBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,6 +42,14 @@ public class MyPageSetProfileActivity extends AppCompatActivity {
                 Intent i = new Intent(MyPageSetProfileActivity.this, SearchAddressActivity.class);
                 startActivityForResult(i, SEARCH_ADDRESS_ACTIVITY);
 
+            }
+        });
+
+        Button btn = (Button)findViewById(R.id.btn_complete_update);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
             }
         });
 
