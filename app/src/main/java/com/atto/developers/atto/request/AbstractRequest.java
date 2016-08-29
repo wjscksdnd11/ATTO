1b694074d25deaf0f70d60ac3aa02993c70fdeab
jeon <wjscksdnd11@naver.com>
2016-08-29 16:44:33 +0900

    데이터 더미서버 메세지 추가

---
+++ app/src/main/java/com/atto/developers/atto/request/AbstractRequest.java
@@ -0,0 +1,22 @@
+package com.atto.developers.atto.request;
+
+import com.atto.developers.atto.manager.NetworkRequest;
+
+import okhttp3.HttpUrl;
+
+/**
+ * Created by Tacademy on 2016-08-29.
+ */
+public abstract class AbstractRequest <T> extends NetworkRequest<T> {
+
+protected HttpUrl.Builder getBaseUrlBuilder() {
+        HttpUrl.Builder builder = new HttpUrl.Builder();
+        builder.scheme("https");
+        builder.host("ec2-52-78-136-23.ap-northeast-2.compute.amazonaws.com");
+        builder.port(443);
+        return builder;
+        }
+
+
+
+}
