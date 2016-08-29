f07665c47d0ff412ac100f36913eff3364966d5e
Park Museong <goodness897@gmail.com>
2016-08-29 16:52:51 +0900

    Daum search address add daum.html SearchAddressActivity

--- app/src/main/java/com/atto/developers/atto/MyPageSetProfileActivity.java
+++ app/src/main/java/com/atto/developers/atto/MyPageSetProfileActivity.java
@@ -2,7 +2,6 @@ package com.atto.developers.atto;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -13,8 +12,7 @@ import android.widget.TextView;
 public class MyPageSetProfileActivity extends AppCompatActivity {
 
     private static final int SEARCH_ADDRESS_ACTIVITY = 10000;
-
-    TextInputEditText inputAddress;
+    TextView addressView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,7 +31,7 @@ public class MyPageSetProfileActivity extends AppCompatActivity {
         TextView textView = (TextView) toolbar.findViewById(R.id.text_title);
         textView.setText(R.string.activity_my_page_set_profile);
 
-        inputAddress = (TextInputEditText) findViewById(R.id.edit_profile_set_address);
+        addressView = (TextView)findViewById(R.id.text_profile_set_address);
 
         Button btn = (Button) findViewById(R.id.btn_complete_update); // 수정 완료 버튼
         btn.setOnClickListener(new View.OnClickListener() {
@@ -43,17 +41,15 @@ public class MyPageSetProfileActivity extends AppCompatActivity {
             }
         });
 
-        btn = (Button) findViewById(R.id.btn_move_address);
-        btn.setOnClickListener(new View.OnClickListener() {
+        ImageButton imgBtn = (ImageButton) findViewById(R.id.btn_img_address);
+        imgBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
                 Intent i = new Intent(MyPageSetProfileActivity.this, SearchAddressActivity.class);
                 startActivityForResult(i, SEARCH_ADDRESS_ACTIVITY);
 
             }
         });
-
 
     }
 
@@ -68,7 +64,7 @@ public class MyPageSetProfileActivity extends AppCompatActivity {
                 if (resultCode == RESULT_OK) {
                     String data = intent.getExtras().getString("data");
                     if (data != null)
-                        inputAddress.setText(data);
+                        addressView.setText(data);
                 }
                 break;
 
