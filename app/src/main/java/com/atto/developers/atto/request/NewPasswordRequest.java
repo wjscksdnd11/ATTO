716800e0dcfbc6b8dec47bd1032ec1f7b02cd544
jeon <wjscksdnd11@naver.com>
2016-09-01 11:11:43 +0900

    request 5번 비밀번호 수정 추가

--- app/src/main/java/com/atto/developers/atto/request/NewPasswordRequest.java
+++ app/src/main/java/com/atto/developers/atto/request/NewPasswordRequest.java
@@ -1,19 +1,19 @@
 package com.atto.developers.atto.request;
 
-import android.util.Log;
-
-import com.atto.developers.atto.networkdata.userdata.MyProfile;
+import com.atto.developers.atto.networkdata.ResultMessage;
 import com.google.gson.reflect.TypeToken;
 
 import java.lang.reflect.Type;
 
+import okhttp3.FormBody;
 import okhttp3.HttpUrl;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 
 /**
- * Created by Tacademy on 2016-08-30.
+ * Created by Tacademy on 2016-09-01.
  */
-public class MyProfileRequest extends AbstractRequest<MyProfile> {
+public class NewPasswordRequest extends AbstractRequest<ResultMessage> {
 
     private final static String MEMBERS = "members";
     private final static String ME ="me";
@@ -22,39 +22,34 @@ public class MyProfileRequest extends AbstractRequest<MyProfile> {
     private final static String PASSWORD = "member_password";
     private final static String NEW_PASSWORD ="member_newpassword";
 
+
     Request mRequest;
 
-
-    public MyProfileRequest() { //내 프로필 조회
-
-        HttpUrl url = getBaseUrlBuilder()
+    public NewPasswordRequest(String password, String new_password) {
+        HttpUrl url  = getBaseUrlHttpsBuilder()
                 .addPathSegment(MEMBERS)
                 .addPathSegment(ME)
                 .build();
-
+        RequestBody body  = new FormBody.Builder()
+                .add(ACTION,ACTION_VALUE)
+                .add(PASSWORD,password)
+                .add(NEW_PASSWORD,new_password)
+                .build();
         mRequest = new Request.Builder()
                 .url(url)
+                .put(body)
                 .build();
 
 
-        Log.i("URL", mRequest.url().toString());
-
-
     }
-
-
-
+    @Override
+    protected Type getType() {
+        return new TypeToken<ResultMessage>(){}.getType();
+    }
 
     @Override
     public Request getRequest() {
         return mRequest;
     }
 
-
-
-
-    @Override
-    protected Type getType() {
-        return new TypeToken<MyProfile>(){}.getType();
-    }
 }
