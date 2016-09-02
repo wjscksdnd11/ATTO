4c58d2de125aa0c620e74ec6b80198e124f00b74
Park Museong <goodness897@gmail.com>
2016-09-02 15:11:55 +0900

    design update

---
+++ app/src/main/java/com/atto/developers/atto/view/VerticalSpaceItemDecoration.java
@@ -0,0 +1,28 @@
+package com.atto.developers.atto.view;
+
+import android.graphics.Rect;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+/**
+ * Created by goodn on 2016-09-02.
+ */
+public class VerticalSpaceItemDecoration extends RecyclerView.ItemDecoration {
+
+    private final int mVerticalSpaceHeight;
+
+    public VerticalSpaceItemDecoration(int mVerticalSpaceHeight) {
+        this.mVerticalSpaceHeight = mVerticalSpaceHeight;
+    }
+
+    @Override
+    public void getItemOffsets(Rect outRect, View view, RecyclerView parent,
+                               RecyclerView.State state) {
+        outRect.bottom = mVerticalSpaceHeight;
+
+        // 마지막 아이템 줄 생략을 원한다면,
+        if (parent.getChildAdapterPosition(view) != parent.getAdapter().getItemCount() - 1) {
+            outRect.bottom = mVerticalSpaceHeight;
+        }
+    }
+}
