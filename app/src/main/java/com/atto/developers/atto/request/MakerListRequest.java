9ae4cae80e197c83227001ee08e3268d1e3fe66c
jeon <wjscksdnd11@naver.com>
2016-09-02 18:58:47 +0900

    21. 제작자 리스트

--- app/src/main/java/com/atto/developers/atto/request/MakerListRequest.java
+++ app/src/main/java/com/atto/developers/atto/request/MakerListRequest.java
@@ -1,8 +1,27 @@
 package com.atto.developers.atto.request;
 
+import com.atto.developers.atto.networkdata.makerdata.MakerData;
+import com.atto.developers.atto.networkdata.tradedata.TradeListData;
+
+import java.lang.reflect.Type;
+
+import okhttp3.Request;
+
 /**
  * Created by Tacademy on 2016-09-02.
  */
 
-//public class MakerListRequest extends AbstractRequest<TradeListData<>> {
-//}
+public class MakerListRequest extends AbstractRequest<TradeListData<MakerData>> {
+    public MakerListRequest() {
+    }
+
+    @Override
+    protected Type getType() {
+        return null;
+    }
+
+    @Override
+    public Request getRequest() {
+        return null;
+    }
+}
