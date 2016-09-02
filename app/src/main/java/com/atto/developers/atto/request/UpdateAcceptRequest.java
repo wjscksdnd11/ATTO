a4864d56fda58444fc9e16f0152e0a3c17516c4a
jeon <wjscksdnd11@naver.com>
2016-09-02 13:51:30 +0900

    request action.txt 설명 정보 추가

---
+++ app/src/main/java/com/atto/developers/atto/request/UpdateAcceptRequest.java
@@ -0,0 +1,14 @@
+package com.atto.developers.atto.request;
+
+/**
+ * Created by Tacademy on 2016-09-02.
+ */
+
+//    action n_accept  일때
+//    accept  : 협상 수락 정보(number) 0 - 미수락 / 1 - 수락
+
+public class UpdateAcceptRequest {
+
+
+
+}
