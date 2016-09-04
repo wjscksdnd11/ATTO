6762e0d002e14dbb3e3f62615ea877f3e334d207
jeon <wjscksdnd11@naver.com>
2016-09-05 08:55:37 +0900

    MypageViewHolder

--- app/src/main/java/com/atto/developers/atto/viewholder/MypageViewHolder.java
+++ app/src/main/java/com/atto/developers/atto/viewholder/MypageViewHolder.java
@@ -2,7 +2,6 @@ package com.atto.developers.atto.viewholder;
 
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
-import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -12,13 +11,13 @@ import com.atto.developers.atto.networkdata.userdata.MyProfileData;
 /**
  * Created by Tacademy on 2016-08-25.
  */
-public class MyPageViewHolder extends RecyclerView.ViewHolder {
+public class MypageViewHolder extends RecyclerView.ViewHolder {
 
 
     ImageView my_page_profile;
     TextView my_page_staus, my_page_title, my_page_price,
             my_page_day, my_page_limit_date, my_page_nickname;
-    ImageButton btn_chat;
+    ImageView btn_chat;
 
     MyProfileData myProfileData;
 
@@ -33,7 +32,7 @@ public class MyPageViewHolder extends RecyclerView.ViewHolder {
         this.listener = listener;
     }
 
-    public MyPageViewHolder(View itemView) {
+    public MypageViewHolder(View itemView) {
         super(itemView);
         my_page_profile = (ImageView) itemView.findViewById(R.id.my_page_img_trade_profile);
         my_page_staus = (TextView) itemView.findViewById(R.id.my_page_text_trade_staus);
@@ -42,7 +41,7 @@ public class MyPageViewHolder extends RecyclerView.ViewHolder {
         my_page_day = (TextView) itemView.findViewById(R.id.my_page_text_trade_dday);
         my_page_limit_date = (TextView) itemView.findViewById(R.id.my_page_text_trade_limit_date);
         my_page_nickname = (TextView) itemView.findViewById(R.id.my_page_text_trade_nickname);
-        btn_chat = (ImageButton) itemView.findViewById(R.id.btn_chat);
+        btn_chat = (ImageView) itemView.findViewById(R.id.btn_chat);
     }
 
     public void setMypageData(MyProfileData myProfileData) {
