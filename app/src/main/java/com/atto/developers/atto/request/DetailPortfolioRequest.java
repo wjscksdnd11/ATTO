6f5fdde804e2455e30e7a093ca199540e2c637eb
jeon <wjscksdnd11@naver.com>
2016-09-06 10:07:54 +0900

    portfolio 검색추가 마무리

---
+++ app/src/main/java/com/atto/developers/atto/request/DetailPortfolioRequest.java
@@ -0,0 +1,43 @@
+package com.atto.developers.atto.request;
+
+import android.content.Context;
+
+import com.atto.developers.atto.networkdata.ResultMessage;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+
+import okhttp3.HttpUrl;
+import okhttp3.Request;
+
+/**
+ * Created by Tacademy on 2016-09-06.
+ */
+public class DetailPortfolioRequest extends AbstractRequest<ResultMessage> {
+Request mRequest;
+
+    private final static String PORTFOLIO = "portfolioes";
+
+    public DetailPortfolioRequest(Context context, String tid) {
+        HttpUrl url = getBaseUrlBuilder()
+                .addPathSegment(PORTFOLIO)
+                .build();
+        mRequest = new Request.Builder()
+
+                .url(url)
+                .tag(context)
+                .build();
+
+    }
+    @Override
+    protected Type getType() {
+        return new TypeToken<ResultMessage>(){}.getType();
+    }
+
+    @Override
+    public Request getRequest() {
+        return mRequest;
+    }
+
+
+}
